13e977ee51aa4f14c85dcb47213d049f40dbb115
Gabriel Jara <149518249+gao4323@users.noreply.github.com>
2026-08-27 04:23:23 +0000

    Crear spring

---
+++ ulasalle/src/main/java/com/actividad1/ulasalle/UlasalleApplication.java
@@ -0,0 +1,13 @@
+package com.actividad1.ulasalle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlasalleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UlasalleApplication.class, args);
+	}
+
+}
---
+++ ulasalle/src/test/java/com/actividad1/ulasalle/UlasalleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actividad1.ulasalle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlasalleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
